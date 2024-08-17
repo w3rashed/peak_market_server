7d084897dea52f948df4e5026c9a0b9d5d9e6430
Rashed <w3rashed@gmail.com>
2024-08-17 13:36:22 +0600

    mongodb user and pass set

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.json());
 // mongodb code here
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://prod_commerce:01305801580@cluster0.mmdewqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrpddgz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,10 +29,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
+    //   -------------------------------------------------
+
+
+
+
+
+
+    // ----------------------------------------------------
+      
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       // Ensures that the client will close when you finish/error
